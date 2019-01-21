edc8a53f2fba7bef10d3da915a767fbb76ddbfdc
panzhiwei <panzhiwei@yy.com>
2019-01-21 19:01:36 +0800

    代码整理

--- app/src/main/java/com/example/vivic/nolost/login/LoginActivity.kt
+++ app/src/main/java/com/example/vivic/nolost/login/LoginActivity.kt
@@ -4,19 +4,21 @@ package com.example.vivic.nolost.login
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import cn.bmob.v3.exception.BmobException
 import cn.sharesdk.sina.weibo.SinaWeibo
 import cn.sharesdk.tencent.qq.QQ
+import com.example.vivic.nolost.bmob.IBmobCallback
 import com.example.vivic.nolost.R
 import com.example.vivic.nolost.activity.BaseActivity
 import com.example.vivic.nolost.bean.MyUser
-import com.example.vivic.nolost.bmob.IBmobCallback
-import com.example.vivic.nolost.bmob.UserRepository
 import com.example.vivic.nolost.commonUtil.NetworkUtil
+import com.example.vivic.nolost.commonUtil.NoDoubleClickListener
 import com.example.vivic.nolost.commonUtil.pref.CommonPref
 import com.example.vivic.nolost.commonUtil.toastUtil.ToastUtil
+import com.example.vivic.nolost.bmob.UserRepository
 import com.xiasuhuei321.loadingdialog.view.LoadingDialog
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_login.*
@@ -64,7 +66,7 @@ class LoginActivity : BaseActivity() {
                 ToastUtil.showToast("请输入完整信息")
                 return@setOnClickListener
             }
-            btn_sign.isEnabled = false
+
             val myUser = MyUser().apply {
                 this.username = et_login_account.text.toString()
                 this.setPassword(et_login_password.text.toString())
@@ -73,13 +75,11 @@ class LoginActivity : BaseActivity() {
                 override fun success(result: MyUser?) {
                     ToastUtil.showToast("sign success,welcome:" + result?.username)
                     EventBus.getDefault().post(UserEvent(true, result))
-                    btn_sign.isEnabled = true
                 }
 
                 override fun error(throwable: Throwable?) {
                     val exception = throwable as BmobException
                     ToastUtil.showToast("sign fail," + exception.toString())
-                    btn_sign.isEnabled = true
                 }
             }))
         }
@@ -112,23 +112,29 @@ class LoginActivity : BaseActivity() {
         }
 
 
-        iv_login_qq.setOnClickListener {
-            if (!NetworkUtil.isConnected()) {
-                ToastUtil.showToast("当前无网络")
-                return@setOnClickListener
-            }
-            currentThirdPlatform = QQ.NAME
-            UserRepository.loginByShareSdk(QQ.NAME, object : IBmobCallback<MyUser> {
-                override fun success(result: MyUser?) {
-                    updateUserInfo(result!!)
+        iv_login_qq.setOnClickListener(object : NoDoubleClickListener() {
+            override fun onNoDoubleClick(v: View) {
+                if (!NetworkUtil.isConnected()) {
+                    ToastUtil.showToast("当前无网络")
+                    return
                 }
+                currentThirdPlatform = QQ.NAME
+                UserRepository.loginByShareSdk(QQ.NAME, object : IBmobCallback<MyUser> {
+                    override fun success(result: MyUser?) {
+                        updateUserInfo(result!!)
+                    }
 
-                override fun error(throwable: Throwable?) {
-                    loadingDialog?.loadFailed()
-                }
-            })
-            loadingDialog?.show()
-        }
+                    override fun error(throwable: Throwable?) {
+                        loadingDialog?.loadFailed()
+                    }
+                })
+                loadingDialog?.show()
+            }
+
+            override fun onDoubleClick() {
+
+            }
+        })
     }
 
     private fun normalLogin() {
@@ -140,7 +146,6 @@ class LoginActivity : BaseActivity() {
             ToastUtil.showToast("请输入完整信息")
             return
         }
-        btn_login.isEnabled = false
         val myUser = MyUser().apply {
             this.username = et_login_account.text.toString()
             this.setPassword(et_login_password.text.toString())
@@ -149,13 +154,11 @@ class LoginActivity : BaseActivity() {
             override fun success(result: MyUser?) {
                 ToastUtil.showToast("login success,welcome:" + result?.username)
                 EventBus.getDefault().post(UserEvent(true, result))
-                btn_login.isEnabled = true
             }
 
             override fun error(throwable: Throwable?) {
                 val exception = throwable as BmobException
                 ToastUtil.showToast("login fail," + exception.toString())
-                btn_login.isEnabled = true
             }
         }))
     }
