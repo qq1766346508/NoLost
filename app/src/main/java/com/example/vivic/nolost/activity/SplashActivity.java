75eb5b24bfec3d4c03ea83b6f8e20a5ee4c78178
panzhiwei <panzhiwei@yy.com>
2019-01-28 15:21:00 +0800

    闪屏页

---
+++ app/src/main/java/com/example/vivic/nolost/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.vivic.nolost.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+}
